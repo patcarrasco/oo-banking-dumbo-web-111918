d3995e417840e7be43c0fe96ff1a5fbacb085cf9
github username <github email address>
2018-11-25 16:52:40 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class BankAccount
+
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amt)
+    self.balance += amt
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def withdraw(amt)
+    self.balance -= amt
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,57 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && status == "pending" && green?(amount)
+      transfer_process
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete" && reverse_green?(amount)
+      reverse_process
+      self.status = "reversed"
+    else
+      "Reverse transaction rejected. Please check your account balance."
+    end
+  end
+
+  private
+
+  def green?(amt)
+    sender_end_bal = self.sender.balance - amt
+    sender_end_bal > 0
+  end
+
+  def reverse_green?(amt)
+    receiver_end_bal = self.receiver.balance - amt
+    receiver_end_bal > 0
+  end
+
+  def reverse_process
+    self.sender.deposit(self.amount)
+    self.receiver.withdraw(self.amount)
+  end
+
+  def transfer_process
+    self.sender.withdraw(self.amount)
+    self.receiver.deposit(self.amount)
+  end
 end
